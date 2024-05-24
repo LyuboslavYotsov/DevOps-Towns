4d32cfd11033a18e16b19660e828167c1db17151
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-05-24 17:48:32 +0300

    Add Town logic implemented

--- Towns/towns.js
+++ Towns/towns.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+	$('#btnDelete').click(deleteTown),
+	$('#btnAdd').click(addTown);
 });
+
+function addTown() {
+	let townName = $('#townNameForAdd').val();
+	$('#townNameForAdd').val('');
+	$('#towns').append($('<option>').text(townName));
+	$('#result').text(townName + " added.");
+}
+
 
 function deleteTown() {
 	let townName = $('#townName').val();
